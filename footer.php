dbcdcbadd67b1aa73b02b54ef58f42207e8102c2
Letiteuf55 <arthur.lemoine@me.com>
2017-06-07 15:48:57 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+
+<footer class="container-fluid text-center">
+  <p>Letiteuf55 Copyright</p>
+  <p><a href="https://www.paypal.me/Letiteuf55" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-thumbs-up"></span> Donnation</a></p>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/featured.php">Featured</a></li>
+        <li><a href="/search.php"><span class='glyphicon glyphicon-search'></span> Search</a></li>
+        <li><a href="/contact.php"><span class='glyphicon glyphicon-envelope'></span> Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/login.php"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
